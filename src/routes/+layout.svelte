fb4866f452abf8005945f98b612c6173e96add34
amartin <alejandro.martin.bizneo@gmail.com>
2022-10-11 17:43:44 +0200

    Added +layout to global styles and a example of a responsive case use

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<slot />
+
+<style>
+  :global(body) {
+    background: rgb(38, 3, 82);
+    display: grid;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,14 @@
 
 <style>
   :global(.svelte-fluid-layout-large main) {
-    width: 66%;
+    width: 100%;
+    height: 3141px;
+    background: rgb(38, 3, 82);
+  }
+
+  :global(.svelte-fluid-layout-small main) {
+    width: 100%;
+    height: 1201px;
+    background: rgb(38, 3, 82);
   }
 </style>
