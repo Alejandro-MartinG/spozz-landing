cb1157f50542b9f17acb6af111fcdb88a659a4f6
amartin <14140693+Alejandro-MartinG@users.noreply.github.com>
2022-10-31 06:34:50 +0100

    Form page styles

--- src/routes/form/+layout.svelte
+++ src/routes/form/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
-  import Footer from "../../components/footer.svelte"
-  import Logo from "../../components/logo.svelte"
-  import formBg from "$lib/images/form_bg.png";
+    import Footer from "../../components/footer.svelte"
+    import Logo from "../../components/logo.svelte"
+    import formBg from "$lib/images/form_bg.png";
 
-  const title = 'Join to SPOZZ.Club'
+    const title = 'Join to SPOZZ.Club'
+
+    $: bgImage = `background-image: url("${formBg}");`;
+    // $: width = `width: ${w}px;`;
+    // $: height = `height: ${h}px;`;
+    // $: bgSize = `background-size: calc(100% * ${rows}) calc(100% * ${cols});`;
+    // $: bgPos = `background-position: ${current * -w}px ${-h}px;`;
 </script>
 
-<main>
-    <img class="background" src={formBg} alt="SvelteKit" />
-
+<main style="{bgImage}">
     <Logo></Logo>
 
     <div class="hb">
@@ -27,30 +31,20 @@
 <style>
     main {
         display: flex;
-        flex-direction: column;
-        max-width: 1920px;
-        max-height: 1080px;
-    }
-    .background {
-        position: absolute;
-        z-index: -1;
-        max-height: 100%;
-        max-width: 100%;
-        height: 100%;
-        width: 100%;
+        flex-direction:column;
+        background-repeat: round;
     }
     .hb {
         display: inline-flex;
         flex-direction: column;
         align-items: flex-end;
+        margin-right: 3rem;
     }
     .hbo {
-        display: inline-flex;
-        flex-direction: column;
-        align-items:center;
+        margin: 6rem 12rem 6rem 6rem;
+        align-self: end;
     }
     span {
-        width: 994px;
         height: 98px;
         font-family: "Montserrat";
         font-weight: "bold";
@@ -63,12 +57,9 @@
         opacity: 0.3;
     }
     .title2 {
-        opacity: 0.8;
-    }
-    .title3 {
         opacity: 0.6;
     }
-    .title4 {
-        opacity: 0.3;
+    .title3 {
+        opacity: 0.8;
     }
 </style>
--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,52 +1,55 @@
 <script>
-    let radioSelector = 'Plain';
+    let radioSelector = '1';
+    let radioSelector2 = '';
     let selected = 'Plain';
+    let disabled = false;
 </script>
 
-<div class="card">
-  <div class="card-header">Choose the role that best describes you</div>
+<div class="cardo">
+  <div class="card-header">CHOOSE THE ROLE THAT BEST DESCRIBES YOU</div>
 
   <div class="card-body">
+    <div class="column-title">CHOOSE THE GENRE THAT BEST DESCRIBES YOUR ART</div>
     <div class="card-radio">
         <label>
-            <input type="radio" bind:group={radioSelector} value="Plain">
+            <input type="radio" bind:group={radioSelector} value="1">
             Artist, Creator
         </label>
 
         <label>
-            <input type="radio" bind:group={radioSelector} value="Whole wheat">
+            <input type="radio" bind:group={radioSelector} value="2">
             Lover of arts, music, photography 
         </label>
 
         <label>
-            <input type="radio" bind:group={radioSelector} value="Spinach">
+            <input type="radio" bind:group={radioSelector} value="3">
             Investor, Trader
         </label>
 
         <label>
-            <input type="radio" bind:group={radioSelector} value="Spinach">
+            <input type="radio" bind:group={radioSelector} value="4">
             Other
         </label>
     </div>
 
-    <div class="card-radio-2 disabled">
+    <div class="card-radio-2">
         <label>
-            <input type="radio" bind:group={radioSelector} value="Plain">
+            <input type="radio" {disabled} bind:group={radioSelector2} value="music">
             Music
         </label>
 
         <label>
-            <input type="radio" bind:group={radioSelector} value="Whole wheat">
+            <input type="radio" bind:group={radioSelector2} value="art">
             Digital Art, Collectibles
         </label>
 
         <label>
-            <input type="radio" bind:group={radioSelector} value="Spinach">
+            <input type="radio" bind:group={radioSelector2} value="film">
             Video Art, Film and Animations
         </label>
 
         <label>
-            <input type="radio" bind:group={radioSelector} value="Spinach">
+            <input type="radio" bind:group={radioSelector2} value="photo">
             Photography
         </label>
     </div>
@@ -56,41 +59,84 @@
 </div>
 
 <style>
-    .card {
+    .cardo {
+        position: relative;
+        margin: auto;
+        max-width: 660px;
+        max-height: 460px;
+        width: 100%;
+        height: 100%;
+
         background: #FFFFFF;
-        mix-blend-mode: normal;
         box-shadow: 4px 4px 20px rgba(0, 0, 0, 0.25);
         border-radius: 21px;
+        overflow:hidden;
+    }
+    .card-body {
         display: grid;
         gap: 0.5rem;
         grid-template-rows: 10% 70% 20%;
         grid-template-columns: 50% 50%;
         padding: 1rem;
-        overflow:hidden;
     }
+    .column-title {
+        padding: 0.4rem 0.8rem 0.4rem 1rem;
+        grid-column: 2;
+        font-size: 13px;
+        line-height: 132.1%;
+        letter-spacing: 0.085em;
+
+        border: 1px solid;
+        border-radius: 150px;
+        border-color: rgba(106, 90, 205, 0.5);
+
+        opacity: 0.8;
+        background: linear-gradient(90deg, #713ACA 0%, #4786D6 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+    }
+
+    .card-header {
+        display: block;
+        text-align: center;
+        color: #fff;
+        background: linear-gradient(90deg, #713ACA 0%, #4786D6 100%);
+    }
+
     .card-radio {
+        margin: .5rem .5rem .5rem 2rem;
         display: flex;
         flex-direction: column;
     }
     .card-radio-2 {
+        margin: .5rem .5rem .5rem 1.7rem;
         display: flex;
         flex-direction: column;
         float: right;
     }
     .btn {
-        font-family: 'Montserrat';
-        font-style: normal;
-        font-weight: 500;
+        margin: auto;
+        grid-row: 3;
+        grid-column-start: 1;
+        grid-column-end: 3;
+        width: 107px;
         font-size: 11px;
         line-height: 132.1%;
         letter-spacing: 0.105em;
         background: linear-gradient(90deg, #713ACA 0%, #4786D6 100%);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
+        border-radius: 5px;
     }
-    .card-header {
-        background: linear-gradient(90deg, #713ACA 0%, #4786D6 100%);
-        mix-blend-mode: normal;
-        transform: matrix(1, 0, 0, 1, 0, 0);
+    label {
+        font-family: 'Montserrat';
+        font-weight: 400;
+        font-size: 15px;
+        line-height: 210%;
+        letter-spacing: 0.000em;
+    }
+    input {
+        accent-color: rgb(93, 53, 252);
     }
 </style>
