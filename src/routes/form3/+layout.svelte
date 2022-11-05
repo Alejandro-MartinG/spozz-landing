924464258266ee1929cb7760d3620d507c12c3f2
amartin <14140693+Alejandro-MartinG@users.noreply.github.com>
2022-11-05 20:48:46 +0100

    Rework Form3 structure to be dynamic

--- src/routes/form3/+layout.svelte
+++ src/routes/form3/+layout.svelte
@@ -4,12 +4,9 @@
     import formBg from "$lib/images/form_bg_3.png";
     import title2 from "$lib/images/join-spozz-linear.png";
     import title1 from "$lib/images/join-spozz.png";
+    import line from "$lib/images/small_line.png";
 
     $: bgImage = `background-image: url("${formBg}");`;
-    // $: width = `width: ${w}px;`;
-    // $: height = `height: ${h}px;`;
-    // $: bgSize = `background-size: calc(100% * ${rows}) calc(100% * ${cols});`;
-    // $: bgPos = `background-position: ${current * -w}px ${-h}px;`;
 </script>
 
 <svelte:body style="background:#260352"></svelte:body>
@@ -17,12 +14,35 @@
     <Logo></Logo>
 
     <div class="hb">
-        <img class="title2" src={title2}  alt="SvelteKit"/>
-        <img class="title1" src={title1}  alt="SvelteKit"/>
+        <img class="title" src={title2}  alt="SvelteKit"/>
+        <img class="title" src={title1}  alt="SvelteKit"/>
     </div>
 
     <div class="hbo">
-        <slot></slot>
+        <slot/>
+
+        <div>
+            <span style="font-weight: bold;">Subscribe to our newsletter and receive regular updates about<br> the SPOZZ.club project by e-mail.<br></span>
+            <form>
+                <label>E-Mail:<input type="email" placeholder="  /enter your email"></label>
+                <a href={null} class="btn-p btn btn-primary">SUBMIT</a>
+            </form>
+        </div>
+
+        <img class="line" src={line}  alt="SvelteKit"/>
+
+        <div>
+            <span class="center-text" style="font-weight: bold;">
+                Thank you!<br>
+                We appreciate your interest in SPOZZ.club very much!<br>
+                Check out our awesome community and become a member today!<br>
+            </span>
+            <div class="btns">
+                <a href={null} class="btn1 btn btn-primary">JOIN OUR DISCORD</a>
+                <a href={null} class="btn1 btn btn-primary">GET A SPOZZ MEMBERSHIP AVATAR</a>
+            </div>
+        </div>
+
     </div>
 
     <Footer></Footer>
@@ -51,10 +71,60 @@
         margin: 3rem 6rem 3rem 6rem;
         align-self: center;
     }
-    .title1 {
+    .title {
         mix-blend-mode: overlay;
     }
-    .title2 {
-        mix-blend-mode: overlay;
+    .btns {
+        display: flex;
+        align-content: center;
+    }
+    .btn1 {
+        margin: auto;
+        margin-top: 1rem;
+        font-size: 12px;
+        line-height: 132%;
+        letter-spacing: 0.105em;
+        border-radius: 5px;
+        border: 1px solid #FFFFFF;
+        background-color: transparent;
+    }
+    .center-text {
+        display: flex;
+        text-align: center;
+        line-height: 30px;
+        text-transform: uppercase;
+    }
+    .btn-p {
+        margin: auto;
+        width: 107px;
+        font-size: 11px;
+        line-height: 132.1%;
+        letter-spacing: 0.105em;
+        background: linear-gradient(90deg, #713ACA 0%, #4786D6 100%);
+        border-radius: 5px;
+        box-shadow: var(--shadow-elevation-medium);
+        border: none;
+    }
+    label {
+        font-family: 'Montserrat';
+        font-weight: 400;
+        font-size: 15px;
+        line-height: 210%;
+        letter-spacing: 0.000em;
+        color: #fff;
+    }
+    input {
+        margin: .5rem;
+        display: inline-flex;
+        accent-color: rgb(93, 53, 252);
+        border-radius: 10px;
+        max-width: 370px;
+        width: 370px;
+        border: none;
+    }
+    .line {
+        width: 100%;
+        max-width: 250px;
+        margin: 2rem 2rem .5rem 2rem;
     }
 </style>
--- src/routes/form3/+page.svelte
+++ src/routes/form3/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import title from "$lib/images/art-title.png";
-    import line from "$lib/images/small_line.png";
+    import invest from "$lib/images/invest-title.png";
 </script>
 
 <img class="title" src={title}  alt="SvelteKit"/>
@@ -19,49 +19,7 @@
   </div>
 </div>
 
-<div>
-    <span style="font-weight: bold;">Subscribe to our newsletter and receive regular updates about<br> the SPOZZ.club project by e-mail.<br></span>
-    <form>
-        <label>E-Mail:<input type="email" placeholder="  /enter your email"></label>
-        <a href={null} class="btn-p btn btn-primary">SUBMIT</a>
-    </form>
-</div>
-
-<img class="line" src={line}  alt="SvelteKit"/>
-
-<div>
-    <span class="center-text" style="font-weight: bold;">
-        Thank you!<br>
-        We appreciate your interest in SPOZZ.club very much!<br>
-        Check out our awesome community and become a member today!<br>
-    </span>
-    <div class="btns">
-        <a href={null} class="btn1 btn btn-primary">JOIN OUR DISCORD</a>
-        <a href={null} class="btn1 btn btn-primary">GET A SPOZZ MEMBERSHIP AVATAR</a>
-    </div>
-</div>
-
 <style>
-    .btns {
-        display: flex;
-        align-content: center;
-    }
-    .btn1 {
-        margin: auto;
-        margin-top: 1rem;
-        font-size: 12px;
-        line-height: 132%;
-        letter-spacing: 0.105em;
-        border-radius: 5px;
-        border: 1px solid #FFFFFF;
-        background-color: transparent;
-    }
-    .center-text {
-        display: flex;
-        text-align: center;
-        line-height: 30px;
-        text-transform: uppercase;
-    }
     .cardo {
         display: flex;
         flex-direction: column;
@@ -90,7 +48,6 @@
 
         color: #5658CF;
     }
-
     .card-header {
         display: block;
         text-align: center;
@@ -101,43 +58,9 @@
         letter-spacing: -0.03em;
         text-transform: uppercase;
     }
-
-    .btn-p {
-        margin: auto;
-        width: 107px;
-        font-size: 11px;
-        line-height: 132.1%;
-        letter-spacing: 0.105em;
-        background: linear-gradient(90deg, #713ACA 0%, #4786D6 100%);
-        border-radius: 5px;
-        box-shadow: var(--shadow-elevation-medium);
-        border: none;
-    }
-    label {
-        font-family: 'Montserrat';
-        font-weight: 400;
-        font-size: 15px;
-        line-height: 210%;
-        letter-spacing: 0.000em;
-        color: #fff;
-    }
-    input {
-        margin: .5rem;
-        display: inline-flex;
-        accent-color: rgb(93, 53, 252);
-        border-radius: 10px;
-        max-width: 370px;
-        width: 370px;
-        border: none;
-    }
     .title {
         mix-blend-mode: overlay;
         align-self: center;
         margin-bottom: 3rem;
-    }
-    .line {
-        width: 100%;
-        max-width: 250px;
-        margin: 2rem 2rem .5rem 2rem;
     }
 </style>
